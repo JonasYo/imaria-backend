bdf8bfcf8ababc8260d2d2a19e4fd05e68066308
Jonas Siqueira Marcelino <jonas.1301721@fapi-pinhais.edu.br>
2019-11-12 20:49:50 -0300

    Configurando Babel e Sucrase

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,5 @@
 const dotenv = require('dotenv');
 
-dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
+dotenv.config({
+  path: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev',
+});
--- src/server.js
+++ src/server.js
@@ -2,4 +2,6 @@ import app from './app';
 
 const port = process.env.PORT || 3000;
 
+console.log(process.env.NODE_ENV);
+
 app.listen(port);
